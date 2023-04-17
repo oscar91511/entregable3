40295b9e030f3fe7ea54b42033ae0e046894bf3f
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-04-17 02:36:13 -0500

    change on div father location and ul 16px

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -6,7 +6,7 @@ const Location = ({location}) => {
 return (
 <section className="max-w-5xl lg:m-auto">
     <h2 className="text-center text-[25px] font-bold text-blue-200">{location?.name}</h2>
-    <ul className="flex justify-evenly text-[20px] py-4 font-fira mx-auto">
+    <ul className="flex justify-evenly text-[16px] py-4 font-fira mx-auto">
         <li className="">Type: {location?.type}</li>
         <li className="">Dimension: {location?.dimension}</li>
         <li className="">Population: {location?.residents.length} </li>
