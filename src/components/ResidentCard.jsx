400009d2de3634aa55064eebbd892aad452d6ca3
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-04-18 11:06:34 -0500

    change on padding on cards and width of text

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -20,7 +20,7 @@ const ResidentCard = ({resident}) => {
 
 
   return (
-    <article className="text-white ">
+    <article className="text-white">
       <div className='relative'>
       <img className='w-full  border-[1px] border-green-500' src={residentInfo?.image} alt="" />
       <div className='border-[2px] border-green-500 absolute bottom-4 left-1/2 -translate-x-1/2 bg-[#020A02]/60 text-white p-1 px-2
@@ -29,20 +29,20 @@ const ResidentCard = ({resident}) => {
         <span>{residentInfo?.status}</span>
       </div>
       </div>
-      <section className=" border-[1px] p-4 shadow-green-500 shadow-lg border-green-500">
+      <section className=" border-[1px] p-2 shadow-green-500 shadow-lg border-green-500">
         <h3 className=" font-Dongle uppercase font-['Fira_Code'] text-blue-300 text-[10px]  text-center">{residentInfo?.name}</h3>
         <ul className=" ">
           <li className="">
-            <span className="text-lime-300 text-[9px] uppercase font-bold">Species :</span>
-            <span className="text-[8px] pl-1  font-bold uppercase"> {residentInfo?.species}</span>
+            <span className="text-lime-300 text-[10px] uppercase font-bold">Species :</span>
+            <span className="text-[10px] pl-1  font-bold uppercase"> {residentInfo?.species}</span>
           </li>
           <li className="">
             <span  className="text-lime-300 text-[10px] font-bold uppercase" >Origin:</span>
-            <span className="text-[8px] pl-1 uppercase">{residentInfo?.origin.name}</span>
+            <span className="text-[9px] pl-1 uppercase">{residentInfo?.origin.name}</span>
           </li>
           <li className="">
             <span  className="text-lime-300 text-[10px] uppercase font-bold">Times appear:</span>
-            <span className="text-[9px] pl-4 uppercase font-bold">{residentInfo?.episode.length}</span>
+            <span className="text-[12px] pl-4 uppercase font-bold">{residentInfo?.episode.length}</span>
           </li>
       
         </ul>
